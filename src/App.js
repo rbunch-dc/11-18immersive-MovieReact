3541b06b48978235864e3a5dc55145f28f77bc57
Robert Bunch <robertbunch@Roberts-MacBook-Pro.local>
2019-02-14 14:36:25 -0500

    add search

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
     this.state = {
       movieList: []
-    }
+	}
+	this.movieSearch = this.movieSearch.bind(this);
   }
 
   componentDidMount(){
@@ -34,6 +35,25 @@ class App extends Component {
 	console.log("Checking... yes! It's mounted");
 
 }
+
+	movieSearch(e){
+		console.log(e)
+		e.preventDefault();
+		console.log("Form submitted!");
+		const movieTitle = document.getElementById('searchTerm').value;
+		const url = 'https://api.themoviedb.org/3/search/movie?api_key=fec8b5ab27b292a68294261bb21b04a5&query='+movieTitle;
+		fetch(url)
+		.then((response)=>{
+		  return response.json();
+		})
+		.then((myJson)=>{
+			const results = myJson.results;
+			console.log(results)
+			this.setState({
+				movieList: results
+			});
+		});		
+	}
   
   render() {
 	  const posters = this.state.movieList.map((movie,i)=>{
@@ -44,6 +64,12 @@ class App extends Component {
     	<div className="container">
 			<div className="row">
 				<h1>The movie app...again.</h1>
+
+				<form onSubmit={this.movieSearch}>
+					<input id="searchTerm" type="text" placeholder="Movie Title" />
+					<button type="submit" className="btn waves-effect btn-light">Search</button>
+				</form>
+
 				{posters}
 			</div>
       	</div>
@@ -52,3 +78,11 @@ class App extends Component {
 }
 
 export default App;
+
+// inside of React...
+// if(this.Component.props.onSubmit){
+// 	// onSubmit handler passed
+// 	// call the function, that was passed
+// 	this.Component.props.onSubmit()
+// }
+
